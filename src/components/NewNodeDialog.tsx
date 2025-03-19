624fad6b8b52d887c9e606bcb2377919af86e1cf
BernardMoy <bernardmoy3@gmail.com>
2025-03-19 15:06:39 +0000

    Dialog for add node

--- src/components/NewNodeDialog.tsx
+++ src/components/NewNodeDialog.tsx
@@ -1,4 +1,12 @@
-import { Dialog, DialogContent, DialogTitle } from "@mui/material";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogActions,
+  TextField,
+  Button,
+} from "@mui/material";
+import { CONTENT_MARGIN } from "./Values";
 
 interface Props {
   open: boolean;
@@ -9,7 +17,29 @@ export default function NewNodeDialog({ open, onClose }: Props) {
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle> Add new node </DialogTitle>
-      <DialogContent>Content goes here</DialogContent>
+
+      {/* The input field of the node name */}
+      <DialogContent>
+        <TextField
+          autoFocus
+          id="nodeNameTextField"
+          variant="outlined"
+          label="Node name"
+          color="primary"
+          type="text"
+          sx={{ my: CONTENT_MARGIN }}
+        />
+      </DialogContent>
+
+      {/* The confirm and dismiss buttons */}
+      <DialogActions>
+        <Button onClick={onClose} variant="outlined" color="primary">
+          Cancel
+        </Button>
+        <Button onClick={onClose} variant="contained" color="primary">
+          Add
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 }
