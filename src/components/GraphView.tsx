2c9d49c883e5d489cee72eb568c78f0910461ce8
BernardMoy <bernardmoy3@gmail.com>
2025-03-21 17:04:17 +0000

    Nodes change on hover

--- src/components/GraphView.tsx
+++ src/components/GraphView.tsx
@@ -1,23 +1,49 @@
 import { useEffect, useRef } from "react";
-import { Network } from "vis-network/standalone";
+import { IdType, Network } from "vis-network/standalone";
+import { NODE_COLOR } from "./Values";
 
 export default function GraphView() {
   const ref = useRef<HTMLDivElement>(null);
+
+  const onHover = function (
+    values: any,
+    id: IdType,
+    selected: boolean,
+    hovering: boolean
+  ) {
+    values.color = "#ffffff";
+  };
 
   useEffect(() => {
     if (!ref.current) {
       return;
     }
 
+    // id is mandatory for nodes
     const nodes = [
-      { id: "A", label: "NodeA" },
-      { id: "B", label: "NodeB" },
-      { id: "C", label: "NodeC" },
+      {
+        id: "A",
+        label: "NodeA",
+        color: NODE_COLOR,
+        chosen: { node: onHover, label: false },
+      },
+      {
+        id: "B",
+        label: "NodeB",
+        color: NODE_COLOR,
+        chosen: { node: onHover, label: false },
+      },
+      {
+        id: "C",
+        label: "NodeC",
+        color: NODE_COLOR,
+        chosen: { node: onHover, label: false },
+      },
     ];
 
     const edges = [
-      { from: "A", to: "B" },
-      { from: "B", to: "C" },
+      { from: "A", to: "B", color: "#ffb6bd" },
+      { from: "B", to: "C", color: "#ffb6bd" },
     ];
 
     // graph options
@@ -27,6 +53,9 @@ export default function GraphView() {
       width: "100%",
       physics: {
         enabled: false,
+      },
+      interaction: {
+        hover: true,
       },
     };
 
--- src/components/Values.tsx
+++ src/components/Values.tsx
@@ -1,3 +1,4 @@
 export const CONTENT_MARGIN = 2;
 export const TITLE_MARGIN = 4;
 export const DRAWER_WIDTH = 240;
+export const NODE_COLOR = "#dceef5";
