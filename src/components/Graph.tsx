6b11f5337799bb27f0f74892c5603d8c2a26b2bc
BernardMoy <bernardmoy3@gmail.com>
2025-03-28 22:36:43 +0000

    Graph shortest path method

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,5 +1,6 @@
 import { Edge, IdType } from "vis-network/standalone";
 import { Line } from "../pages/Home";
+import FastPriorityQueue from "fastpriorityqueue";
 
 // define the type stored in the adjacency list (DestinationID, weight, Line)
 interface Destination {
@@ -108,7 +109,67 @@ export class Graph {
   }
 
   // method to generate the shortest route between two ids
-  findShortestRoute(startNodeID: string, endNodeID: string): IdType[] {
-    return [];
+  findShortestRoute(startNodeID: IdType, endNodeID: IdType): IdType[] {
+    // distance of all nodes to the source
+    let d = new Map<IdType, number>();
+    d.set(startNodeID, 0); // mark source as distance 0
+
+    // map to store node to their parent
+    let parents = new Map<IdType, IdType | null>();
+    parents.set(startNodeID, null); // mark source as parent = null
+
+    // priority queue
+    let q = new FastPriorityQueue<{ distance: number; currentNode: IdType }>(
+      function (a: any, b: any) {
+        return a.distance < b.distance; // distance as the comparator
+      }
+    );
+    let visited = new Set<IdType>();
+
+    // while q is non empty
+    while (!q.isEmpty()) {
+      // pop from the queue
+      let { distance, currentNode } = q.poll()!;
+
+      // skip if node is visited or not in graph (Sink node)
+      if (visited.has(currentNode) || !this.adj.has(currentNode)) {
+        continue;
+      }
+
+      // iterate the neighbours of the node in the graph
+      for (let { node, weight, line } of this.adj.get(currentNode)!) {
+        // newly visited neighbour
+        if (!d.has(node)) {
+          d.set(node, d.get(currentNode)! + weight);
+        } else {
+          d.set(node, Math.min(d.get(node)!, d.get(currentNode)! + weight));
+        }
+
+        // push the neighbour to the heap
+        q.add({ distance: d.get(node)!, currentNode: node });
+
+        // mark the parent of node as currentNode
+        parents.set(node, currentNode);
+      }
+
+      // mark current node as visited
+      visited.add(currentNode);
+    }
+
+    // if the end node is not in the parents map, it is unreachable
+    if (!parents.has(endNodeID)) {
+      return []; // return empty list if not reachable
+    }
+
+    // extract the route by iterating the parents in reverse
+    let route: IdType[] = [];
+    let currentParent: IdType = endNodeID;
+    while (currentParent != null) {
+      route.unshift(currentParent); // add parent to front
+      currentParent = parents.get(currentParent)!;
+    }
+
+    // return the final route in reverse
+    return route;
   }
 }
